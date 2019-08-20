ed3a855a3cf8bf7ecad34c517a86f3e85ee71a6c
Steve Adams <irwtdvoys@gmail.com>
2019-08-20 18:32:00 +0100

    Add options property to InputMask

--- src/Api/Request/InputMask.php
+++ src/Api/Request/InputMask.php
@@ -10,24 +10,27 @@
 		public $name;
 		public $type;
 		public $children = array();
-		
+		public $options = array();
+
 		public function __construct($data = null)
 		{
 			parent::__construct($data);
 		}
-		
+
 		public function add($name, $type = null, $options = array())
 		{
 			$structure = array(
 				"name" => $name,
-				"type" => $type
+				"type" => $type,
+				"options" => $options
 			);
-		
 			$this->children[] = new InputMask($structure);
-			
+
+
+
 			return $this;
 		}
-		
+
 		public function inflate($fields)
 		{
 			foreach ($fields as $key => $value)
@@ -44,7 +47,7 @@
 					$this->children[] = $mask;
 				}
 			}
-			
+
 			return $this;
 		}
 	}
